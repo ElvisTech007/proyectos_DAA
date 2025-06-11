e1a6c1f1776fc88068d428266ee2f9eb2d5d5447
Emiliano Aguilar Echeverría <emiliano.aguilarech@uaem.edu.mx>
2025-06-11 17:31:59 -0600

    Implementado el filtro y de regreso

--- proyecto_FFT/main_script_proyecto_6.py
+++ proyecto_FFT/main_script_proyecto_6.py
@@ -41,7 +41,8 @@ try:
     #sns.lineplot(audio_data)
     #plt.show()
     audio_data = audio_potencia(audio_data)
-
+    # print(len(audio_data))
+    # quit()
     # El algoritmo funciona con listas de python
     # entonces casteamos:
     print("Señal original:", audio_data[5:11])
@@ -65,41 +66,54 @@ try:
     audio_reconstruido = audio_reconstruido.astype(OG_DATATYPE)
     wavfile.write("./AUDIO_RECONSTRUIDO.wav", rate, audio_reconstruido)
 
-    # Ahora vamos a generar la grafica:
-    # vamos a hacer la comparativa de antes, transofrmado y despues
-    fig, axes = plt.subplots(3,1,figsize=(12, 10))
-    # Para la grafica de la señal original
-    tiempo = np.arange(len(audio_data))/rate
-    axes[0].set_title("Audio original")
-    axes[0].set_xlabel("Tiempo (s)")
-    axes[0].set_xlabel("Amplitud (s)")
-    axes[0].grid(True) 
-    sns.lineplot(x=tiempo, y =audio_data*MAX_INT, ax=axes[0])
+    # # Ahora vamos a generar la grafica:
+    # # vamos a hacer la comparativa de antes, transofrmado y despues
+    # fig, axes = plt.subplots(3,1,figsize=(12, 10))
+    # # Para la grafica de la señal original
+    # tiempo = np.arange(len(audio_data))/rate
+    # axes[0].set_title("Audio original")
+    # axes[0].set_xlabel("Tiempo (s)")
+    # axes[0].set_xlabel("Amplitud (s)")
+    # axes[0].grid(True) 
+    # sns.lineplot(x=tiempo, y =audio_data*MAX_INT, ax=axes[0])
 
-    # Para la grafica de las frecuencias
-    # Aquí hay que hacer algunas cositas
-    # primero debemos calcular que frecuencias tiene nuestra señal
+    # # Para la grafica de las frecuencias
+    # # Aquí hay que hacer algunas cositas
+    # # primero debemos calcular que frecuencias tiene nuestra señal
     frecuencias = np.fft.fftfreq(len(audio_fft), 1/rate)
-    # Misma señal original
-    frecuencias_positivas = frecuencias[:len(audio_fft)//2]
-    # Arreglo de las magnitudes, al sacarle valor asboluto 
-    # a un complejo sacas su modulo:
-    magnitudes = np.abs(np.array(audio_fft[:len(audio_fft)//2]))
+    # # Misma señal original
+    # frecuencias_positivas = frecuencias[:len(audio_fft)//2]
+    # # Arreglo de las magnitudes, al sacarle valor asboluto 
+    # # a un complejo sacas su modulo:
+    # magnitudes = np.abs(np.array(audio_fft[:len(audio_fft)//2]))
 
-    sns.lineplot(x=frecuencias_positivas, y=magnitudes, ax=axes[1])
+    # sns.lineplot(x=frecuencias_positivas, y=magnitudes, ax=axes[1])
 
-    axes[1].set_title('Espectro de POTENCIA de la FFT') # Cambia el título
-    axes[1].set_xlabel('Frecuencia (Hz)')
-    axes[1].set_ylabel('Potencia') # O 'Magnitud Cuadrada'
-    axes[1].set_xlim(0, rate / 2)
-    axes[1].grid(True)
+    # axes[1].set_title('Espectro de amplitud de la FFT') # Cambia el título
+    # axes[1].set_xlabel('Frecuencia (Hz)')
+    # axes[1].set_ylabel('Potencia') # O 'Magnitud Cuadrada'
+    # axes[1].set_xlim(0, rate / 2)
+    # axes[1].grid(True)
 
-    sns.lineplot(x=tiempo, y=audio_reconstruido, ax=axes[2])
-    axes[2].set_title("Audio original")
-    axes[2].set_xlabel("Tiempo (s)")
-    axes[2].set_xlabel("Amplitud (s)")
-    axes[2].grid(True)
-    plt.subplots_adjust(hspace=0.5)
-    plt.show()
+    # sns.lineplot(x=tiempo, y=audio_reconstruido, ax=axes[2])
+    # axes[2].set_title("Audio original")
+    # axes[2].set_xlabel("Tiempo (s)")
+    # axes[2].set_xlabel("Amplitud (s)")
+    # axes[2].grid(True)
+    # plt.subplots_adjust(hspace=0.5)
+    # plt.show()
+
+    # Ahora vamos a modificar la señal
+    frecuencia_corte = 2000
+    # Mascara binaria de 1 y 0,
+    filtro = np.abs(frecuencias) >= frecuencia_corte
+    audio_fft_filtrado = audio_fft * filtro
+    # Ahora realizamos la inversa:
+    audio_filtrado = ifft(audio_fft_filtrado)
+    # Ahora pasamos las frecuencias a audio de nuevo:
+    audio_filtrado_reconstruido = (np.array(audio_filtrado)/len(audio_data)).real
+    audio_filtrado_reconstruido *= MAX_INT
+    audio_filtrado_reconstruido = audio_filtrado_reconstruido.astype(OG_DATATYPE)
+    wavfile.write("./AUDIO_FILTRDO_RECONSTRUIDO.wav", rate, audio_filtrado_reconstruido)
 except FileNotFoundError:
     print(f"No existe el archivo xd")
